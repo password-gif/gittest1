0b88a0f22aa7bf62ed1e8f17d0fe07e609651855
password-gif <84270596+password-gif@users.noreply.github.com>
2021-09-01 20:19:42 +0800

    test

--- src/main/java/com/itheima/pojo/User.java
+++ src/main/java/com/itheima/pojo/User.java
@@ -1,40 +1,13 @@
 package com.itheima.pojo;
 
 public class User {
-    private int id;
-    private String name;
-    private String password;
+    private int age;
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", password='" + password + '\'' +
-                '}';
+    public int getAge() {
+        return age;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
+    public void setAge(int age) {
+        this.age = age;
     }
 }
