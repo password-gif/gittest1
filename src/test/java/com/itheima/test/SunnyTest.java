5ff94caffc25483c51a4314062d43ec286356945
password-gif <84270596+password-gif@users.noreply.github.com>
2021-09-02 08:55:25 +0800

    test

--- src/test/java/com/itheima/test/SunnyTest.java
+++ src/test/java/com/itheima/test/SunnyTest.java
@@ -1,4 +1,14 @@
 package com.itheima.test;
 
 public class SunnyTest {
+
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
